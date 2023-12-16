a2c5e784c2c2d0eb754b78334a6c87f9534064bd
Luiz Henrique <luizhenriqueccarvalho@gmail.com>
2023-12-16 14:21:44 -0300

    base do banco pronta

--- core/admin.py
+++ core/admin.py
@@ -1,27 +1,63 @@
-from django.utils.html import format_html
 from django.contrib import admin
+from .models import (Usuario, Aluno, AutoAvaliacao, Professor, Curso, 
+                     Disciplina, TurmaDisciplina, Avaliacao, Turma, Criterio, AvaliacoesAluno)
 
-from .models import Professor, Atividade, Turma
+# Utilize os decorators para registrar os modelos
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ['user_nome', 'user_prof_id', 'user_alu_id']
 
 
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ['alu_nome', 'alu_email', 'alu_num_matricula']
+    search_fields = ['alu_nome', 'alu_email']
+    list_filter = ('alu_nome',)
+
+
+@admin.register(AutoAvaliacao)
+class AutoAvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ['auto_nome', 'auto_peso', 'auto_id_alu']
+
+
+@admin.register(Professor)
 class ProfessorAdmin(admin.ModelAdmin):
-    list_display = ('nome_professor', 'disciplina')
+    list_display = ['prof_nome', 'prof_sobrenome', 'prof_telefone']
+    search_fields = ['prof_nome', 'prof_sobrenome']
 
 
-class AtividadeAdmin(admin.ModelAdmin):
-    list_display = ('nome_atividade', 'descricao', 'data_entrega')
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ['curso_nome']
 
 
+@admin.register(Disciplina)
+class DisciplinaAdmin(admin.ModelAdmin):
+    list_display = ['disc_nome', 'curso_id']
+
+
+@admin.register(TurmaDisciplina)
+class TurmaDisciplinaAdmin(admin.ModelAdmin):
+    list_display = ['disc_id', 'prof_id']
+
+
+@admin.register(Avaliacao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ['aval_nome', 'aval_valortotal', 'aval_data']
+    list_filter = ('aval_data',)
+
+
+@admin.register(Turma)
 class TurmaAdmin(admin.ModelAdmin):
-    list_display = ('turma_codigo', 'aluno_quantidade', 'numero_sala_turma', 'turma_turno', 'display_atividades')
-
-    def display_atividades(self, obj):
-        atividades = obj.atividades.all()
-        return format_html(', '.join(a.nome_atividade for a in atividades))
-
-    display_atividades.short_description = 'Atividades'
+    list_display = ['tur_nome', 'tur_sala', 'tur_turno']
 
 
-admin.site.register(Professor, ProfessorAdmin)
-admin.site.register(Atividade, AtividadeAdmin)
-admin.site.register(Turma, TurmaAdmin)
+@admin.register(Criterio)
+class CriterioAdmin(admin.ModelAdmin):
+    list_display = ['crit_nome', 'crit_peso', 'crit_aval_id']
+
+
+@admin.register(AvaliacoesAluno)
+class AvaliacoesAlunoAdmin(admin.ModelAdmin):
+    list_display = ['aval_alun_crit_id', 'aval_alun_nota']
--- core/models.py
+++ core/models.py
@@ -1,58 +1,126 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
 
+class Usuario(models.Model):
+    user_nome = models.OneToOneField(User, on_delete=models.CASCADE, related_name='usuario_nome')
+    user_senha = models.OneToOneField(User, on_delete=models.CASCADE, related_name='usuario_senha')
+    user_prof_id = models.ForeignKey('Professor', on_delete=models.SET_NULL, null=True, blank=True)
+    user_alu_id = models.ForeignKey('Aluno', on_delete=models.SET_NULL, null=True, blank=True)
+
+class Aluno(models.Model):
+    alu_nome = models.CharField(max_length=255)
+    alu_email = models.EmailField(unique=True)
+    alu_senha = models.IntegerField()  
+    prof_foto = models.ImageField(upload_to='fotos_professores/', default='static/images/sem_foto.jpg')
+    alu_num_matricula = models.IntegerField()
+    
+    def __str__(self):
+        return self.alu_nome
+    
+    # Se a imagem existir, ela será exibida. Caso contrário, a imagem padrão sem_foto.jpg será exibida:
+    def foto_tag(self):
+        if self.foto:
+            return mark_safe('<img src="{}" width="50" height="50" />'.format(self.foto.url))
+        else:
+            default_image_path = 'core/images/sem_foto.jpg'
+            return mark_safe('<img src="{}" width="50" height="50" />'.format(default_image_path))
+
+    foto_tag.short_description = 'Imagem'
+
+    # Verifica se a foto está em branco e, se estiver, atribui o caminho da imagem padrão
+    def save(self, *args, **kwargs):
+        if not self.foto:
+            self.foto = 'alunos/sem_foto.jpg'
+
+        super(Aluno, self).save(*args, **kwargs)
+
+class AutoAvaliacao(models.Model):
+    auto_nome = models.CharField(max_length=255)
+    auto_peso = models.FloatField()
+    auto_id_alu = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.auto_nome
 
 class Professor(models.Model):
-    prof_nome = models.CharField('Nome do professor',max_length=100)
-    prof_disciplina = models.CharField('Disciplina do Professor', max_length=20, choices=[
-        ('geografia', 'Geografia'), ('historia', 'História'), ('matematica', 'Matemática'), ('geometria', 'Geometria'), ('portugues', 'Português'), ('ingles', 'Inglês'), ('fisica', 'Física'), ('quimica', 'Química')])
-    prof_foto = models.ImageField(
-        'Foto', upload_to='professores/', null=True, blank=True)
+    prof_nome = models.CharField(max_length=255)
+    prof_sobrenome = models.CharField(max_length=255)
+    prof_foto = models.ImageField(upload_to='fotos_professores/', default='static/images/sem_foto.jpg')
+    prof_telefone = models.CharField(max_length=20)
 
     def __str__(self):
         return self.prof_nome
     
-class Aluno(models.Model):
-    alu_nome = models.CharField('Nome do aluno',max_length=100)
-    alu_email = models.CharField('Email do aluno',max_length=100)
-    alu_senha = models.CharField('Senha do aluno',max_length=100)
-    prof_disciplina = models.CharField('Disciplina do Professor', max_length=20, choices=[
-        ('geografia', 'Geografia'), ('historia', 'História'), ('matematica', 'Matemática'), ('geometria', 'Geometria'), ('portugues', 'Português'), ('ingles', 'Inglês'), ('fisica', 'Física'), ('quimica', 'Química')])
-    prof_foto = models.ImageField(
-        'Foto', upload_to='professores/', null=True, blank=True)
+     # Se a imagem existir, ela será exibida. Caso contrário, a imagem padrão sem_foto.jpg será exibida:
+    def foto_tag(self):
+        if self.foto:
+            return mark_safe('<img src="{}" width="50" height="50" />'.format(self.foto.url))
+        else:
+            default_image_path = 'core/images/sem_foto.jpg'
+            return mark_safe('<img src="{}" width="50" height="50" />'.format(default_image_path))
+
+    foto_tag.short_description = 'Imagem'
+
+    # Verifica se a foto está em branco e, se estiver, atribui o caminho da imagem padrão
+    def save(self, *args, **kwargs):
+        if not self.foto:
+            self.foto = 'professores/sem_foto.jpg'
+
+        super(Professor, self).save(*args, **kwargs)
+
+class Curso(models.Model):
+    curso_nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.curso_nome
+
+class Disciplina(models.Model):
+    disc_nome = models.CharField(max_length=255)
+    curso_id = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.disc_nome
+
+class TurmaDisciplina(models.Model):
+    disc_id = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    prof_id = models.ForeignKey(Professor, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.alu_nome
 
-
-class Atividade(models.Model):
-    nome_atividade = models.CharField('Nome da Atividade',max_length=100)
-    descricao = models.TextField('Descrição da Atividade')
-    pontos = models.IntegerField('Quantos pontos valem a atividade')
-    data_entrega = models.DateField('Data de Entrega')
+class Avaliacao(models.Model):
+    aval_nome = models.CharField(max_length=255)
+    aval_valortotal = models.FloatField()
+    aval_disc_id = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    aval_prof_id = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    aval_tur_id = models.ForeignKey('Turma', on_delete=models.CASCADE) # 'Turma' está entre aspas pois a classe Turma ainda não foi definida
+    aval_data = models.DateTimeField()
 
     def __str__(self):
-        return self.nome_atividade
-
+        return self.aval_nome
 
 class Turma(models.Model):
-
-    turma_codigo = models.CharField('Codigo da turma', max_length=100)
-    numero_sala_turma = models.IntegerField('Numero da sala da turma')
-    turma_turno = models.CharField('Turno', max_length=20, choices=[
+    tur_nome = models.CharField(max_length=255)
+    tur_sala = models.IntegerField()
+    tur_turno = models.CharField('Turno', max_length=20, choices=[
         ('matutino', 'Matutino'), ('vespertino', 'Vespertino'), ('noturno', 'Noturno')])
-    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
-    atividades = models.ManyToManyField(Atividade)
 
     def __str__(self):
-        return f"Turma {self.turma_codigo}"
-    
-class AutoAvaliacao(models.Model):
-    auto_nome = models.CharField('Nome da Atividade',max_length=100)
-    descricao = models.TextField('Descrição da Atividade')
-    pontos = models.IntegerField('Quantos pontos valem a atividade')
-    data_entrega = models.DateField('Data de Entrega')
+        return self.tur_nome
+
+class Criterio(models.Model):
+    crit_nome = models.CharField(max_length=255)
+    crit_descricao = models.CharField(max_length=1024)
+    crit_peso = models.FloatField()
+    crit_aval_id = models.ForeignKey(Avaliacao, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.nome_atividade
-    
+        return self.crit_nome
 
+class AvaliacoesAluno(models.Model):
+    aval_alun_crit_id = models.ForeignKey(Criterio, on_delete=models.CASCADE)
+    aval_alun_nota = models.FloatField()
+
+    def __str__(self):
+        return self.aval_alun_crit_id
